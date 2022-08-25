cb07fa3c71ce42a61bdb051cfcb228b4672a4f9d
HowePersonal <howe2004new@gmail.com>
2022-08-24 22:56:06 -0700

    Fixed window transition exit error

--- UserInterface.py
+++ UserInterface.py
@@ -2,30 +2,40 @@ import blocker
 import sys
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QStackedWidget
+from PyQt5.QtWidgets import QDialog, QApplication, QStackedWidget
 
 
 class HomeWindow(QDialog):
-    def __init__(self, widget):
+    def __init__(self):
         super().__init__()
-        self.widget = widget
+        self.initUI()
+
+    def initUI(self):
         loadUi("ui/home.ui", self)
         self.btn_blockedHistory.clicked.connect(self.goBlockedWindow)
 
+
+
     def goBlockedWindow(self):
-        blocked_sites_win = BlockedSitesWindow(self.widget)
-        self.widget.addWidget(blocked_sites_win)
-        self.widget.setCurrentIndex(self.widget.currentIndex()+1)
+        win = BlockedSitesWindow()
+        widget.addWidget(win)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+
 
 
 class BlockedSitesWindow(QDialog):
-    def __init__(self, widget):
-        self.widget = widget
+    def __init__(self):
         super().__init__()
+        self.initUI()
+
+    def initUI(self):
         loadUi("ui/blockedhistory.ui", self)
         self.tableWidget.setColumnWidth(0, 319)
         self.tableWidget.setColumnWidth(1, 319)
         self.loaditems()
+        self.btn_home.clicked.connect(self.goHomeWindow)
+
     def loaditems(self):
         blocked_sites = blocker.blocked_IPS()
         row=0
@@ -36,13 +46,25 @@ class BlockedSitesWindow(QDialog):
             self.tableWidget.setItem(row, 1, QtWidgets.QTableWidgetItem(ip))
             row += 1
 
+    def goHomeWindow(self):
+        win = HomeWindow()
+        widget.addWidget(win)
+        widget.setCurrentIndex(widget.currentIndex()-1)
+
+
+
+
+
 
 
 def start():
     app = QApplication(sys.argv)
+    global widget
     widget = QStackedWidget()
-    win = HomeWindow(widget)
-    widget.addWidget(win)
+
+    homeWin = HomeWindow()
+    widget.addWidget(homeWin)
+
     widget.setFixedHeight(500)
     widget.setFixedWidth(800)
     widget.show()
