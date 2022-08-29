3bdc829722d7fb5d172aad268f5aeddbfc1caeda
HowePersonal <howe2004new@gmail.com>
2022-08-28 19:45:17 -0700

    Fixed blocked list website bug that displays multiple versions of the same blocked site

--- blocker.py
+++ blocker.py
@@ -1,7 +1,12 @@
 import os
 
+def read_file():
+    with open('C:/Windows/System32/drivers/etc/hosts', 'r') as file:
+        lines = file.readlines()
+    return lines
+
 def close_browsers():
-    if os.system('tasklist | find "msedge.exe"') == 0: os.system("taskkill /im msedge.exe /f")
+    #if os.system('tasklist | find "msedge.exe"') == 0: os.system("taskkill /im msedge.exe /f")
     if os.system('tasklist | find "chrome.exe"') == 0: os.system("taskkill /im chrome.exe /f")
     if os.system('tasklist | find "firefox.exe"') == 0: os.system("taskkill /im firefox.exe /f")
     if os.system('tasklist | find "opera.exe"') == 0: os.system("taskkill /im opera.exe /f")
@@ -9,15 +14,14 @@ def close_browsers():
 
 def blocked_IPS():
     blocked_ip = {}
-    for line in open('C:/Windows/System32/drivers/etc/hosts', 'r'):
-        if "#" not in line and not line.isspace():
+    for line in read_file():
+        if "#" not in line and not line.isspace() and "www" not in line:
             split_ip = line.split()
             blocked_ip[split_ip[1]] = split_ip[0]
     return blocked_ip
 
 def delete_block(hostname):
-    with open('C:/Windows/System32/drivers/etc/hosts', 'r') as file:
-        lines = file.readlines()
+    lines = read_file()
     with open('C:/Windows/System32/drivers/etc/hosts', 'w') as file:
         for line in lines:
             if hostname not in line:
@@ -26,11 +30,13 @@ def delete_block(hostname):
 def add_block(hostname):
     with open('C:/Windows/System32/drivers/etc/hosts', 'a') as file:
         if "www." in hostname:
-            file.write("0.0.0.0" + "    " + hostname[3:] + "\n")
             file.write("0.0.0.0" + "    " + hostname + "\n")
+            file.write("0.0.0.0" + "    " + hostname[3:] + "\n")
         elif "www." not in hostname:
             file.write("0.0.0.0" + "    " + hostname + "\n")
             file.write("0.0.0.0" + "    " + "www." + hostname + "\n")
-        file.write("::1     " + hostname + "\n")
+        file.write("::1     " + hostname + "\n\n")
+
+
 
 
